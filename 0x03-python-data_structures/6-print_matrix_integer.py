5a6575f426480ab3d8a0c6c645302e3653d37a2c
Levi Eze <somtolevi.eze@gmail.com>
2022-08-27 07:49:07 +0000

    Py Data Structures:-Lists,Tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+able file 10 lines (9 sloc) 332 Bytes
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
